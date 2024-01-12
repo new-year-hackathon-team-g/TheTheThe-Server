98ef8f7348b04bccc04b6e07a8817203cb3e4cd7
Yehyeok Bang <qkddpgur318@gmail.com>
2024-01-12 15:47:52 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/gdsc/hackathon/thethethe/TheTheTheApplication.java
@@ -0,0 +1,13 @@
+package gdsc.hackathon.thethethe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTheTheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTheTheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdsc/hackathon/thethethe/TheTheTheApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.hackathon.thethethe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTheTheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
